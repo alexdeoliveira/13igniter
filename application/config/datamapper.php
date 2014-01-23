d75b69aac253d141e73325769b1eead899a94e80
Maragonha <diego.maragonha@gmail.com>
2014-01-23 12:02:12 -0200

    correcao na library datamapper funcao excluir

--- application/config/datamapper.php
+++ application/config/datamapper.php
@@ -8,18 +8,19 @@
 
 $config['prefix'] = '';
 $config['join_prefix'] = '';
-$config['error_prefix'] = '<p class="text-danger">';
+$config['error_prefix'] = '<p class="help-inline">';
 $config['error_suffix'] = '</p>';
 $config['created_field'] = 'data_cadastro';
 $config['updated_field'] = 'data_edicao';
 $config['local_time'] = FALSE;
 $config['unix_timestamp'] = FALSE;
+$config['cascade_delete'] = FALSE;
 $config['timestamp_format'] = 'Y-m-d H:i:s';
 $config['lang_file_format'] = 'model_${model}';
 $config['field_label_lang_format'] = '${model}_${field}';
-$config['auto_transaction'] = FALSE;
-$config['auto_populate_has_many'] = FALSE;
-$config['auto_populate_has_one'] = FALSE;
+$config['auto_transaction'] = TRUE;
+$config['auto_populate_has_many'] = TRUE;
+$config['auto_populate_has_one'] = TRUE;
 $config['all_array_uses_ids'] = FALSE;
 // set to FALSE to use the same DB instance across the board (breaks subqueries)
 // Set to any acceptable parameters to $CI->database() to override the default.
@@ -27,7 +28,7 @@ $config['db_params'] = '';
 // Uncomment to enable the production cache
 // $config['production_cache'] = 'datamapper/cache';
 $config['extensions_path'] = 'datamapper';
-$config['extensions'] = array('array');
+$config['extensions'] = array('array', 'htmlform', 'custom_rules');
 
 /* End of file datamapper.php */
 /* Location: ./application/config/datamapper.php */
