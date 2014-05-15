42a60391eadf68c1fa2ec345ec3b17a37bb89cc2
Maragonha <diego.maragonha@gmail.com>
2014-05-15 10:43:14 -0300

    Página de 404

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,62 +1,63 @@
+<?php $CI =& get_instance(); ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<title>404 Page Not Found</title>
-<style type="text/css">
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php echo isset($title) ? $title.' | ' : NULL; ?><?php echo $CI->config->item('site_description')?>">
+		<meta name="author" content="13 Bits">
+		<link rel="shortcut icon" href="<?php echo assets_url('img/favicon.ico'); ?>">
 
-::selection{ background-color: #E13300; color: white; }
-::moz-selection{ background-color: #E13300; color: white; }
-::webkit-selection{ background-color: #E13300; color: white; }
+		<title>Página não encontrada | <?php echo $CI->config->item('entidade_nome_breve'); ?></title>
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+		<link rel="shortcut icon" href="<?php echo assets_url('img/favicon.ico'); ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo assets_url('bootstrap/dist/css/bootstrap.min.css'); ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo assets_url('css/style.css'); ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo assets_url('css/v-1.0.css'); ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo assets_url('fontello/css/ignitercms.css'); ?>">
+		<!--[if IE 7]><link rel="stylesheet" href="<?php echo assets_url('fontello/css/ignitercms-ie7.css'); ?>"><![endif]-->
+		
+		<!-- Chosen v1.0.0 -->
+		<!-- Custom styles for this template -->
+		<link href="<?php echo assets_url('css/offcanvas.css') ?>" rel="stylesheet">
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+		<?php if (isset($assets_css) AND is_array($assets_css)): ?>
+			<?php foreach ($assets_css as $row): ?>
+				<?php echo $row; ?>
+			<?php endforeach ?>
+		<?php endif ?>
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="<?php echo assets_url('bootstrap/assets/js/html5shiv.js') ?>"></script>
+			<script src="<?php echo assets_url('bootstrap/assets/js/respond.min.js') ?>"></script>
+		<![endif]-->
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+		<?php if(ENVIRONMENT !== 'development'): ?>
+			<script type="text/javascript">
+				var _gaq = _gaq || [];
+				_gaq.push(['_setAccount', '<?php echo $CI->config->item('ga_codigo'); ?>']);
+				_gaq.push(['_trackPageview']);
+				(function() {
+					var ga = document.createElement('script');
+					ga.type = 'text/javascript';
+					ga.async = true;
+					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					var s = document.getElementsByTagName('script')[0];
+					s.parentNode.insertBefore(ga, s);
+				})();
+			</script>
+		<?php endif; ?>
+	</head>
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
-</head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
+	<body class="">
+		<div class="container" style="margin-top:150px">
+			<div class="well text-center">
+				<h1>Oops!</h1>
+				<p class="lead">Página não encontrada</p>
+				<p>O endereço abaixo não existe <br/> <strong><?php echo current_url(); ?></strong></p>
+				<a href="<?php echo base_url(); ?>" class="btn btn-primary">Página inicial</a>
+			</div>
+		</div>
+	</body>
 </html>
