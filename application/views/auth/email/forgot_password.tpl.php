b6bc8872704df2261f006e2d8e474d55638bf7f0
Maragonha <diego@13bits.com.br>
2014-11-17 12:02:07 -0200

    Template para esqueci a senha

--- application/views/auth/email/forgot_password.tpl.php
+++ application/views/auth/email/forgot_password.tpl.php
@@ -1,6 +1,15 @@
 <html>
 <body>
-	<h1>Reset Password for <?php echo $identity;?></h1>
-	<p>Please click this link to <?php echo anchor('auth/reset_password/'. $forgotten_password_code, 'Reset Your Password');?>.</p>
+	<h1>Redefinição de senha para <?php echo $identity;?></h1>
+	<p>Por favor, clique neste link para <?php echo anchor('auth/reset_password/'. $forgotten_password_code, 'Redefinir sua senha');?>.</p>
+	<p>
+		Atenciosamente, <br>
+		<?php echo $this->config->item('entidade_nome_breve') ?>
+	</p>
+	<hr>
+	<p>
+		Esta mensagem foi enviada automaticamente pela <?php echo $this->config->item('entidade_nome_breve') ?>, por favor não responda.<br>
+		<a href="<?php echo base_url() ?>"><?php echo base_url() ?></a>
+	</p>
 </body>
 </html>
