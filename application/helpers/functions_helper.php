0b3b220d3f2318ce26d77396071364d58a66d4ea
Vinícius Ribas Samuel dos Santos <vinirssantos@gmail.com>
2015-09-17 09:20:44 -0300

    add função para envio de email

--- application/helpers/functions_helper.php
+++ application/helpers/functions_helper.php
@@ -28,3 +28,55 @@ if (! function_exists('string_limiter')) {
 		return $string;
 	}
 }
+
+/**
+ * Envia email
+ * @param string $to
+ * @param string $subject
+ * @param string $message
+ * @param string $reply
+ * @param string $cc
+ * @return boolean 
+ */
+if ( ! function_exists('send_email')) 
+{
+	function send_email($to, $subject, $message, $reply='', $cc='') 
+	{
+		if (empty($to) AND empty($subject) AND empty($message)) {
+			return false;
+		}
+
+		$CI =& get_instance();
+		$CI->load->config('email');
+
+		$from = $CI->config->item('entidade_email_contato');
+		$entidade_nome_breve = $CI->config->item('entidade_nome_breve');
+		//titulo default
+		$title = "<h3><strong>".$subject."</strong></h3>";
+		//assinatura default
+		$subscription = "
+			<br /><br />---<br />
+			Atenciosamente,<br />
+			<a href=\"".base_url()."\">".base_url()."</a>
+		";
+
+		$CI->load->library('email');
+		//monta email
+		$CI->email->from($from, 'Site '.$entidade_nome_breve);
+		$CI->email->to($to);
+		if (!empty($reply)) {
+			$CI->email->reply_to($reply, 'Site '.$entidade_nome_breve);
+		}
+		if (!empty($cc)) {
+			$CI->email->cc($cc);
+		}
+		$CI->email->subject($subject);
+		$CI->email->message($title.$message.$subscription);
+		//envia email
+		if ($CI->email->send()) {
+			return true;
+		}
+		
+		return false;
+	}
+}
